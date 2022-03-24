6020868602b54339ad677d506b987ea527654c54
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-03-24 13:26:47 +0000

    access port number form process.env object

--- server/listen.js
+++ server/listen.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 
-app.listen(9090, (error) => {
+app.listen((process.env.PORT || 9090), (error) => {
     if (error) console.log(error);
     else console.log("server is listening");
 });
