7484fb00da381e3250296f0c72236413f531c99e
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2022-03-24 13:32:41 +0000

    Minor refactors for efficiency

--- database/seeds/seed.js
+++ database/seeds/seed.js
@@ -1,16 +1,12 @@
 const { dropTables, createTables, insertData } = require("../utils");
 
 const seed = async ({ topicData, userData, articleData, commentData }) => {
-	try {
-		await dropTables();
-		await createTables();
-		await insertData(topicData, "topics");
-		await insertData(userData, "users");
-		await insertData(articleData, "articles");
-		await insertData(commentData, "comments");
-	} catch (error) {
-		console.log(error);
-	}
+	await dropTables();
+	await createTables();
+	await insertData(topicData, "topics");
+	await insertData(userData, "users");
+	await insertData(articleData, "articles");
+	await insertData(commentData, "comments");
 };
 
 module.exports = seed;
